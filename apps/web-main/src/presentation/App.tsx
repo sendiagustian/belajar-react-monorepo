840ad7c3c4be13e2a6a4752863a2dda65e900afc
sendiagustian <sendiagustian.pk@gmail.com>
2025-03-11 13:06:28 +0700

    update web main card and addded footer

--- apps/web-main/src/presentation/App.tsx
+++ apps/web-main/src/presentation/App.tsx
@@ -1,13 +1,25 @@
+import { Box } from "@repo/ui/MaterialUI";
+import Footer from "../../../../libs/ui/src/core/Footer";
 import { Header } from "../../../../libs/ui/src/core/Header";
 import { Catalogues } from "./components/Catalogues";
 // import "./App.css";
 
 function App() {
     return (
-        <>
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                minHeight: "100vh",
+                pt: 8,
+            }}
+        >
             <Header />
-            <Catalogues />
-        </>
+            <Box sx={{ flex: 1, display: "flex", flexDirection: "column" }}>
+                <Catalogues />
+            </Box>
+            <Footer />
+        </Box>
     );
 }
 
