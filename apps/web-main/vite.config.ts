8ba62665642c24fc99ce41aca0063cdf6d2d120a
sendiagustian <sendiagustian.pk@gmail.com>
2025-03-12 15:56:21 +0700

    up conf

--- apps/web-main/vite.config.ts
+++ apps/web-main/vite.config.ts
@@ -3,6 +3,25 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
+    const proxy = {
+        "/react-essentials": {
+            target:
+                mode === "development" ? "http://localhost:5511" : "http://react-essentials:5511",
+            changeOrigin: true,
+        },
+        "/tic-tac-toe": {
+            target: mode === "development" ? "http://localhost:5512" : "http://tic-tac-toe:5512",
+            changeOrigin: true,
+        },
+        "/investment-calculator": {
+            target:
+                mode === "development"
+                    ? "http://localhost:5513"
+                    : "http://investment-calculator:5513",
+            changeOrigin: true,
+        },
+    };
+
     return {
         plugins: [react()],
         base: "/",
@@ -10,29 +29,13 @@ export default defineConfig(({ mode }) => {
             port: 5510,
             host: "0.0.0.0",
             strictPort: true,
-            proxy: {
-                "/react-essentials": {
-                    target:
-                        mode === "development"
-                            ? "http://localhost:5511"
-                            : "http://react-essentials:5511",
-                    changeOrigin: true,
-                },
-                "/tic-tac-toe": {
-                    target:
-                        mode === "development"
-                            ? "http://localhost:5512"
-                            : "http://tic-tac-toe:5512",
-                    changeOrigin: true,
-                },
-                "/investment-calculator": {
-                    target:
-                        mode === "development"
-                            ? "http://localhost:5513"
-                            : "http://investment-calculator:5513",
-                    changeOrigin: true,
-                },
-            },
+            proxy: proxy,
+        },
+        preview: {
+            port: 5510,
+            host: "0.0.0.0",
+            strictPort: true,
+            proxy: proxy,
         },
     };
 });
