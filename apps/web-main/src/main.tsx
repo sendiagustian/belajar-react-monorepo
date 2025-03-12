5d33afc557a3e0e86b0d5a3b0c549b24142e199c
sendiagustian <sendiagustian.pk@gmail.com>
2025-03-12 11:42:44 +0700

    update

--- apps/web-main/src/main.tsx
+++ apps/web-main/src/main.tsx
@@ -1,14 +1,15 @@
 import { createRoot } from "react-dom/client";
 import { CssBaseline, ThemeProvider } from "@repo/ui/MaterialUI";
 import { webMainTheme } from "@repo/ui/core/themes/WebMainTheme";
-import { BrowserRouter } from "react-router-dom";
-import AppRouter from "./presentation/AppRouter";
+// import { BrowserRouter } from "react-router-dom";
+// import AppRouter from "./presentation/AppRouter";
+import App from "./presentation/App";
 
 createRoot(document.getElementById("root")!).render(
-    <BrowserRouter>
-        <ThemeProvider theme={webMainTheme}>
-            <CssBaseline />
-            <AppRouter />
-        </ThemeProvider>
-    </BrowserRouter>
+    // <BrowserRouter>
+    <ThemeProvider theme={webMainTheme}>
+        <CssBaseline />
+        <App />
+    </ThemeProvider>
+    // </BrowserRouter>
 );
