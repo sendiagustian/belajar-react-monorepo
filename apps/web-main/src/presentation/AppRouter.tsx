99eb74b5a834c8c34893aea02f8d2ddb97ee88f5
sendiagustian <sendiagustian.pk@gmail.com>
2025-03-12 11:05:41 +0700

    update config again

--- apps/web-main/src/presentation/AppRouter.tsx
+++ apps/web-main/src/presentation/AppRouter.tsx
@@ -1,14 +1,24 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import App from "./App";
+
+const ExternalRedirect = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        window.location.href = location.pathname; // Redirect ke path yang sama
+    }, [location]);
+
+    return null; // Tidak menampilkan apapun
+};
 
 const AppRouter: React.FC = () => {
     return (
         <Routes>
             <Route index path="/" element={<App />} />
-            <Route path="/react-essentials" />
-            <Route path="/tic-tac-toe" />
-            <Route path="/investment" />
+            <Route path="/react-essentials" element={<ExternalRedirect />} />
+            <Route path="/tic-tac-toe" element={<ExternalRedirect />} />
+            <Route path="/investment" element={<ExternalRedirect />} />
         </Routes>
     );
 };
