e1f922ee29ad17bb560a77935f7b3e1193069075
sendiagustian <sendiagustian.pk@gmail.com>
2025-03-15 22:44:27 +0700

    up

--- apps/react-essentials/vite.config.ts
+++ apps/react-essentials/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/react-essentials",
+    base: "/react-essentials/",
     server: {
         port: 5511,
         host: "0.0.0.0",
