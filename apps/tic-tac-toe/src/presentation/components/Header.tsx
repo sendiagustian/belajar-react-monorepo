44ea454d4c23ce4053568ef6a7e115d4c5be0e99
sendiagustian <sendiagustian.pk@gmail.com>
2025-03-15 21:09:29 +0700

    up

--- apps/tic-tac-toe/src/presentation/components/Header.tsx
+++ apps/tic-tac-toe/src/presentation/components/Header.tsx
@@ -1,10 +1,11 @@
 import "../../assets/css/header.css";
+import GameLogo from "../../assets/game-logo.png";
 import React from "react";
 
 const Header: React.FC = () => {
     return (
         <header>
-            <img src="game-logo.png" alt="Hand-drawn Tic Tac Toe logo" />
+            <img src={GameLogo} alt="Hand-drawn Tic Tac Toe logo" />
             <h1>Tic Tac Toe</h1>
         </header>
     );
